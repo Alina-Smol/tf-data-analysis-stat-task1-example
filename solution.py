9ebd5a8df36084f4200d089f31a82157c42058c5
Alina Smolina <78797567+Alina-Smol@users.noreply.github.com>
2023-04-07 20:41:50 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,15 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5441234011 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    n = len(x)
+    sigma = np.exp(1)
+    error = np.random.normal(-35, sigma, n)
+    x = error/ 10
+    
     return x.mean() # Ваш ответ
